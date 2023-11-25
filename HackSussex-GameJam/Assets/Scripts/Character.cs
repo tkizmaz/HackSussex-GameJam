09498d4a8ba8c6da46f36fa8e26afb92c38115a0
Taha Kızmaz <tahakizmaz@gmail.com>
2023-11-25 21:09:35 +0000

    feat(Weapon): Add weapon prefabs

---
+++ HackSussex-GameJam/Assets/Scripts/Character.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    private int health = 100;
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
